318e0a7c718c61cebdf6f0f0f9179387854a14cf
muhamed <201834016740@univ-tebessa.dz>
2024-05-14 14:20:32 +0100

    speech to text works

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  FlutterSoundRecorder _recorder = FlutterSoundRecorder();
+  stt.SpeechToText _speech = stt.SpeechToText();
+  late String _tempPath;
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  @override
+  void dispose() {
+    _recorder.closeRecorder();
+    super.dispose();
+  }
+
+  Future<void> _initialize() async {
+    // Request microphone permissions
+    var status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException("Microphone permission not granted");
+    }
+
+    // Initialize the recorder
+    await _recorder.openRecorder();
+
+    // Prepare temporary file path
+    Directory tempDir = await getTemporaryDirectory();
+    _tempPath = '${tempDir.path}/temp.wav';
+  }
+
+  Future<void> playAudio(Source audioPath) async {
+    final result = await audioPlayer.play(audioPath);
+  }
+
+  Future<void> startRecording() async {
+    if (!_recorder.isRecording) {
+      await _recorder.startRecorder(
+        toFile: _tempPath,
+        codec: Codec.pcm16WAV,
+      );
+      setState(() {
+        _isRecording = true;
+      });
+    }
+  }
+
+  Future<void> stopRecording() async {
+    if (_recorder.isRecording) {
+      await _recorder.stopRecorder();
+      setState(() {
+        _isRecording = false;
+      });
+      comparePronunciation(_tempPath);
+    }
+  }
+
+  Future<void> comparePronunciation(String filePath) async {
+    File audioFile = File(filePath);
+    List<int> audioBytes = audioFile.readAsBytesSync();
+    String audioBase64 = base64Encode(audioBytes);
+
+    var request = {
+      'config': {
+        'encoding': 'LINEAR16',
+        'sampleRateHertz': 16000,
+        'languageCode': 'en-US',
+      },
+      'audio': {
+        'content': audioBase64,
+      },
+    };
+
+    var response = await http.post(
+      Uri.parse(
+          'https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyBT_oqU1eZE7MDGuJpsnAOH1Z8ewQ1wslo'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(request),
+    );
+
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(response.body);
+      if (jsonResponse['results'] != null &&
+          jsonResponse['results'].isNotEmpty) {
+        String recognizedText =
+            jsonResponse['results'][0]['alternatives'][0]['transcript'];
+        print('Recognized Text: $recognizedText');
+        // Implement your own comparison logic here
+      } else {
+        print('No speech recognized');
+      }
+    } else {
+      print('Error: ${response.statusCode} ${response.reasonPhrase}');
+      print('Response body: ${response.body}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Non-Word Repetition Task'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => playAudio(AssetSource('sibad.wav')),
+                child: Text('Play Correct Pronunciation'),
+              ),
+              ElevatedButton(
+                onPressed: _isRecording ? stopRecording : startRecording,
+                child:
+                    Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
