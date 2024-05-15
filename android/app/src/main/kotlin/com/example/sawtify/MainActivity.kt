e3e528bf0531a1f967d0190805675015f5629a63
amiraboualleg <boualleg.amira.2000@gmail.com>
2024-05-15 15:07:37 +0200

    real project

---
+++ android/app/src/main/kotlin/com/example/sawtify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sawtify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'utils/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: kPrimaryColor,
+              fontFamily: 'Montserrat',
+            ),
+      ),
+   home: const LoginScreen(),
+       );
+  }
+}
