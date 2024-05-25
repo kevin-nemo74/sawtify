1dd687af15a82dbb5f330a8160d648c5cee85e6f
muhamed <201834016740@univ-tebessa.dz>
2024-05-25 11:06:12 +0100

    test works

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -7,26 +7,29 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class TestPage extends StatefulWidget {
   @override
   _TestPageState createState() => _TestPageState();
 }
 
-class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin {
+class _TestPageState extends State<TestPage>
+    with SingleTickerProviderStateMixin {
   final AudioPlayer _audioPlayer = AudioPlayer();
   bool _isPlaying = false;
   int? _currentPlayingIndex;
-  Set<int> _playedIndices = {}; 
-    Set<int> _recordedIndices = {}; 
+  Set<int> _playedIndices = {};
+  Set<int> _recordedIndices = {};
   late Timer _stopRecordingTimer;
-
   bool _isRecording = false;
-
   late AnimationController _animationController;
   final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
-  List<bool> _isRecordingList = List.generate(28, (index) => false); 
-// List to track recording status
+  List<bool> _isRecordingList = List.generate(28, (index) => false);
+
+  // Store comparison results
+  List<bool> _comparisonResults = List.generate(28, (index) => false);
 
   @override
   void initState() {
@@ -64,11 +67,15 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
     return file.path;
   }
 
+  Future<String> _getRecordedFilePath(int index) async {
+    final directory = await getTemporaryDirectory();
+    return '${directory.path}/audio_example_$index.wav';
+  }
+
   void _playAudio(String assetPath, int index) async {
     if (_playedIndices.contains(index)) {
-      return; 
+      return;
     }
-    
 
     if (_isPlaying && _currentPlayingIndex != index) {
       await _audioPlayer.stop();
@@ -85,47 +92,115 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
       setState(() {
         _isPlaying = true;
         _currentPlayingIndex = index;
-        _playedIndices.add(index); 
+        _playedIndices.add(index);
       });
     }
   }
 
   void _startRecording(int index) async {
     if (await Permission.microphone.request().isGranted) {
-      // Start recording only if it's not already recording
       if (!_isRecordingList[index]) {
-        await _recorder.startRecorder(toFile: 'audio_example_$index.wav');
-        setState(() {
-          // Set recording status for the current item
-          _isRecordingList[index] = true;
-          // Update the recording status
-          _isRecording = true;
-        });
+        try {
+          String filePath = await _getRecordedFilePath(index);
+          await _recorder.startRecorder(toFile: filePath);
+          setState(() {
+            _isRecordingList[index] = true;
+            _isRecording = true;
+          });
 
-        // Start the timer to stop recording after 2 seconds
-        _stopRecordingTimer = Timer(Duration(seconds: 2), () {
-          _stopRecording(index);
-        });
+          _stopRecordingTimer = Timer(Duration(seconds: 2), () {
+            _stopRecording(index);
+          });
+        } catch (e) {
+          print('Error starting recording: $e');
+        }
       }
     }
   }
 
-   void _stopRecording(int index) async {
-    // Stop recording only if it's recording
+  void _stopRecording(int index) async {
     if (_isRecordingList[index]) {
-      await _recorder.stopRecorder();
-      setState(() {
-        // Set recording status for the current item
-        _isRecordingList[index] = false;
-        // Update the recording status
-        _isRecording = false;
-        // Add the index to the recorded indices set when recording finishes for the first time
-        _recordedIndices.add(index);
-      });
+      try {
+        await _recorder.stopRecorder();
+        setState(() {
+          _isRecordingList[index] = false;
+          _isRecording = false;
+          _recordedIndices.add(index);
+        });
+        _compareAudioAndStoreResult(index);
+      } catch (e) {
+        print('Error stopping recording: $e');
+      }
     }
   }
 
+  Future<void> _compareAudioAndStoreResult(int index) async {
+    try {
+      String recordedFilePath = await _getRecordedFilePath(index);
+      String targetAudioPath =
+          await _getFilePath('assets/audios/audio${index + 1}.wav');
 
+      File recordedFile = File(recordedFilePath);
+      if (await recordedFile.exists()) {
+        File targetAudio = File(targetAudioPath);
+        double distance = await _compareAudio(recordedFile, targetAudio);
+        setState(() {
+          _comparisonResults[index] = distance < 0.5;
+        });
+      } else {
+        throw Exception('Recorded file does not exist');
+      }
+    } catch (e) {
+      print('Error during comparison: $e');
+    }
+  }
+
+  Future<double> _compareAudio(File userAudio, File targetAudio) async {
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('https://sawtify-7esi5utsqq-uc.a.run.app/compare'));
+    request.files
+        .add(await http.MultipartFile.fromPath('user_audio', userAudio.path));
+    request.files.add(
+        await http.MultipartFile.fromPath('target_audio', targetAudio.path));
+
+    var response = await request.send();
+    var responseData = await response.stream.toBytes();
+    var responseString = String.fromCharCodes(responseData);
+
+    try {
+      var result = jsonDecode(responseString);
+      print(result);
+      return result['distance'];
+    } catch (e) {
+      print('Error parsing response: $e');
+      print('Response was: $responseString');
+      throw Exception('Failed to parse server response');
+    }
+  }
+
+  void _calculateAndShowResults() {
+    int correctCount = _comparisonResults.where((result) => result).length;
+    double percentage = (correctCount / 28) * 100;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Test Results"),
+          content: Text(
+              "You got $correctCount out of 28 correct.\nPercentage: ${percentage.toStringAsFixed(2)}%"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -137,7 +212,7 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
           children: [
             SizedBox(height: 40),
             Text(
-              ' Test',
+              'Test',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -176,7 +251,8 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
                     scrollDirection: Axis.horizontal,
                     itemCount: 28,
                     itemBuilder: (context, index) {
-                      List<String> audioFiles = List.generate(28, (index) => 'assets/audios/audio${index + 1}.wav');
+                      List<String> audioFiles = List.generate(
+                          28, (index) => 'assets/audios/audio${index + 1}.wav');
                       String audioFilePath = audioFiles[index];
 
                       return AnimationConfiguration.staggeredList(
@@ -187,7 +263,8 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
                           child: FadeInAnimation(
                             child: Container(
                               width: 250,
-                              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
                               child: Card(
                                 shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(20.0),
@@ -198,16 +275,17 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
                                     borderRadius: BorderRadius.circular(20.0),
                                     boxShadow: [
                                       BoxShadow(
-                                        color: Color(0xFF8EAEF1).withOpacity(0.5), 
+                                        color:
+                                            Color(0xFF8EAEF1).withOpacity(0.5),
                                         spreadRadius: 2,
                                         blurRadius: 4,
-                                        offset: Offset(0, 3), 
+                                        offset: Offset(0, 3),
                                       ),
                                     ],
                                     gradient: LinearGradient(
                                       colors: [
-                                        Color(0xFF8EAEF1), // #8EAEF1
-                                        Color(0xFFE4E7F6), // #E4E7F6
+                                        Color(0xFF8EAEF1),
+                                        Color(0xFFE4E7F6),
                                       ],
                                       begin: Alignment.topLeft,
                                       end: Alignment.bottomRight,
@@ -216,11 +294,16 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
                                   child: Padding(
                                     padding: const EdgeInsets.all(16.0),
                                     child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
                                       children: [
                                         SizedBox(height: 10),
                                         Text(
-                                          (index == 0 || index == 11 || index == 21) ? 'Practice' : 'Test',
+                                          (index == 0 ||
+                                                  index == 11 ||
+                                                  index == 21)
+                                              ? 'Practice'
+                                              : 'Test',
                                           style: TextStyle(
                                             fontSize: 20.0,
                                             fontWeight: FontWeight.bold,
@@ -233,70 +316,95 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
                                           height: 120,
                                           fit: BoxFit.cover,
                                           image: AssetImage(
-                                            (index == 0 || index == 11 || index == 21)
+                                            (index == 0 ||
+                                                    index == 11 ||
+                                                    index == 21)
                                                 ? 'assets/Avatar/ph.png'
                                                 : 'assets/Avatar/phh.png',
                                           ),
                                         ),
                                         SizedBox(height: 20),
                                         Row(
-                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
                                           children: [
-      IconButton(
-        icon: ShaderMask(
-          shaderCallback: (Rect bounds) {
-            return LinearGradient(
-              colors: [
-                Colors.white,
-                Color.fromARGB(210, 128, 54, 174),
-              ],
-            ).createShader(bounds);
-          },
-          child: Icon(
-            _playedIndices.contains(index) 
-                ? Icons.block 
-                : (_currentPlayingIndex == index && _isPlaying 
-                    ? Icons.pause
-                    : Icons.play_arrow),
-          ),
-        ),
-        onPressed: _playedIndices.contains(index) 
-            ? null
-            : () => _playAudio(audioFilePath, index),
-        iconSize: 50,
-        color: Colors.white,
-      ),
-    GestureDetector(
-        onTapDown: (_) => _startRecording(index),
-        onTapUp: (_) => _stopRecording(index),
-        child: ScaleTransition(
-          scale: Tween(begin: 1.0, end: 1.2).animate(
-            CurvedAnimation(
-              parent: _animationController,
-              curve: Curves.easeInOut,
-            ),
-          ),
-          child: _recordedIndices.contains(index) 
-              ? Icon(
-                  Icons.block,
-                  color: Colors.grey, 
-                  size: 40,
-                )
-              : _isRecordingList[index]
-                  ? Icon(
-                      Icons.mic, 
-                      color: Color.fromARGB(255, 54, 108, 244), 
-                      size: 40,
-                    )
-                  : Icon(
-                      Icons.mic, 
-                      color: Colors.white, 
-                      size: 40,
-                    ),
-        ),
-      ),
-
-
+                                            IconButton(
+                                              icon: ShaderMask(
+                                                shaderCallback: (Rect bounds) {
+                                                  return LinearGradient(
+                                                    colors: [
+                                                      Colors.white,
+                                                      Color.fromARGB(
+                                                          210, 128, 54, 174),
+                                                    ],
+                                                  ).createShader(bounds);
+                                                },
+                                                child: Icon(
+                                                  _playedIndices.contains(index)
+                                                      ? Icons.block
+                                                      : (_currentPlayingIndex ==
+                                                                  index &&
+                                                              _isPlaying
+                                                          ? Icons.pause
+                                                          : Icons.play_arrow),
+                                                ),
+                                              ),
+                                              onPressed:
+                                                  _playedIndices.contains(index)
+                                                      ? null
+                                                      : () => _playAudio(
+                                                          audioFilePath, index),
+                                              iconSize: 50,
+                                              color: Colors.white,
+                                            ),
+                                            GestureDetector(
+                                              onTapDown: (_) =>
+                                                  _startRecording(index),
+                                              onTapUp: (_) =>
+                                                  _stopRecording(index),
+                                              child: ScaleTransition(
+                                                scale:
+                                                    Tween(begin: 1.0, end: 1.2)
+                                                        .animate(
+                                                  CurvedAnimation(
+                                                    parent:
+                                                        _animationController,
+                                                    curve: Curves.easeInOut,
+                                                  ),
+                                                ),
+                                                child: _recordedIndices
+                                                        .contains(index)
+                                                    ? Icon(
+                                                        Icons.block,
+                                                        color: Colors.grey,
+                                                        size: 40,
+                                                      )
+                                                    : _isRecordingList[index]
+                                                        ? Icon(
+                                                            Icons.mic,
+                                                            color:
+                                                                Color.fromARGB(
+                                                                    255,
+                                                                    54,
+                                                                    108,
+                                                                    244),
+                                                            size: 40,
+                                                          )
+                                                        : Icon(
+                                                            Icons.mic,
+                                                            color: Colors.white,
+                                                            size: 40,
+                                                          ),
+                                              ),
+                                            ),
+                                            IconButton(
+                                              icon: Icon(
+                                                Icons.check_circle_outline,
+                                                color: Colors.white,
+                                                size: 40,
+                                              ),
+                                              onPressed: null,
+                                            ),
                                           ],
                                         ),
                                       ],
@@ -318,13 +426,12 @@ class _TestPageState extends State<TestPage> with SingleTickerProviderStateMixin
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: MaterialButton(
-                  onPressed: () {
-                    // Add your logic to calculate and display the test result
-                  },
+                  onPressed: _calculateAndShowResults,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30.0),
                   ),
-                  padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
                   color: Color.fromARGB(80, 1, 51, 200),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
